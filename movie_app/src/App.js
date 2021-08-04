ce9a48ea16903cad1d5b7e7890f21b66322b2576
hhshhhskk <hhshhhskk@naver.com>
2021-08-04 11:14:31 +0900

    first commit

---
+++ movie_app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Food({favourite}){
+  return <h1>I like {favourite}</h1>;
+}
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food favourite="kimchi" />
+      <Food favourite="ramen" />
+      <Food favourite="samgiopsal" />
+      <Food favourite="chukumi" />
+    </div>
+  );
+}
+
+export default App;
