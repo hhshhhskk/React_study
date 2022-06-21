8c72a28aa3587f48671f638feefde7d76932359b
hhshhhskk <hhshhhskk@naver.com>
2022-06-21 19:05:18 +0900

    react-master: styled components study

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -1,19 +1,50 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+const Emoji = styled.span`
+  font-size: 36px;
 `;
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
 `;
+
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal"/>
-      <Box bgColor="tomato"/>
+      <Box>
+        <Emoji>⚾️</Emoji>
+      </Box>
+        <Emoji>🏀</Emoji>
     </Father>
   );
 }
