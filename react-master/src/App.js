14049c8d9701973d814ea8ca872ec8929fef01b6
hhshhhskk <hhshhhskk@naver.com>
2022-06-23 16:07:21 +0900

    react-master: theme darkTheme & lightTheme

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -1,51 +1,24 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
-const Father = styled.div`
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content:center;
-  align-items:center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 
 function App() {
   return (
-    <Father>
-      <Box>
-        <Emoji>⚾️</Emoji>
-      </Box>
-        <Emoji>🏀</Emoji>
-    </Father>
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
   );
 }
 
